a6a544edc4c556697f7b292c403565f4b701ffa3
deepak.kumar2 <pdeepak1255@gmail.com>
2025-02-04 23:52:00 +0530

    State management, learning about spacing modifier

--- app/src/main/java/com/example/superapp/basic_modifiers/SpacingModifierDemo.kt
+++ app/src/main/java/com/example/superapp/basic_modifiers/SpacingModifierDemo.kt
@@ -1,2 +1,9 @@
 package com.example.superapp.basic_modifiers
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SpacingModifierDemo(modifier: Modifier = Modifier) {
+    
+}
