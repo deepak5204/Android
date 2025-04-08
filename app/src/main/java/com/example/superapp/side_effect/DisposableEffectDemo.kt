4de314a1a9135903e14c359e6aa0b9f312c8c120
deepak.kumar2 <pdeepak1255@gmail.com>
2025-04-08 14:53:57 +0530

    Side Effects, DisposableEffect understand disposable effect execution and onDispose execution

--- app/src/main/java/com/example/superapp/side_effect/DisposableEffectDemo.kt
+++ app/src/main/java/com/example/superapp/side_effect/DisposableEffectDemo.kt
@@ -15,20 +15,31 @@ fun DisposableEffectDemo(modifier: Modifier = Modifier) {
         val observer = LifecycleEventObserver { _, event ->
             when(event){
                 Lifecycle.Event.ON_CREATE -> {
-                    println("ON_CREATE is called")
+                    println("DisposableEffect: ON_CREATE is called")
                 }
-                else -> Unit
-//                Lifecycle.Event.ON_START -> TODO()
-//                Lifecycle.Event.ON_RESUME -> TODO()
-//                Lifecycle.Event.ON_PAUSE -> TODO()
-//                Lifecycle.Event.ON_STOP -> TODO()
-//                Lifecycle.Event.ON_DESTROY -> TODO()
-//                Lifecycle.Event.ON_ANY -> TODO()
+                Lifecycle.Event.ON_START -> {
+                    println("DisposableEffect: Lifecycle Event: ON_START")
+                }
+                Lifecycle.Event.ON_RESUME -> {
+                    println("DisposableEffect: Lifecycle Event: ON_RESUME")
+                }
+                Lifecycle.Event.ON_PAUSE -> {
+                    println("DisposableEffect: Lifecycle Event: ON_PAUSE")
+                }
+                Lifecycle.Event.ON_STOP -> {
+                    println("DisposableEffect: Lifecycle Event: ON_STOP")
+                }
+                Lifecycle.Event.ON_DESTROY -> {
+                    println("DisposableEffect: Lifecycle Event: ON_DESTROY")
+                }
+                Lifecycle.Event.ON_ANY -> {
+                    println("DisposableEffect: Lifecycle Event: ON_ANY")
+                }
             }
         }
         lifecycleOwner.lifecycle.addObserver(observer)
         onDispose {
-            println("onDispose is called")
+            println("DisposableEffect: onDispose is called")
             lifecycleOwner.lifecycle.removeObserver(observer )
         }
     }
