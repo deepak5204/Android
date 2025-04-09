0da11dc8b77d087e86dd9aced0df6cd1bb89b1c8
deepak.kumar2 <pdeepak1255@gmail.com>
2025-04-09 13:40:07 +0530

    Clickable modifier and interaction source

---
+++ app/src/main/java/com/example/superapp/basic_modifiers/ClickableModifiersDemo.kt
@@ -0,0 +1,51 @@
+package com.example.superapp.basic_modifiers
+
+import androidx.compose.foundation.LocalIndication
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.superapp.ui.theme.SuperAppTheme
+import androidx.compose.runtime.getValue
+
+@Composable
+fun ClickableModifiersDemo(modifier: Modifier = Modifier) {
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressed by interactionSource.collectIsPressedAsState()
+    Box(
+        modifier = modifier
+            .size(100.dp)
+            .background(
+                if(isPressed) Color.Blue else Color.White)
+            .clickable (
+                interactionSource,
+                LocalIndication.current
+            ){
+                println("Hello World Clicked")
+            },
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Hello World"
+        )
+    }
+}
+
+
+@Preview
+@Composable
+private fun ClickableModifiersDemoPreview() {
+    SuperAppTheme {
+        ClickableModifiersDemo()
+    }
+}
