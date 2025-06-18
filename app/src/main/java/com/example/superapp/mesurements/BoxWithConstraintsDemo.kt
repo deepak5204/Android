8d12f3dfd95c83973fad99705a8ec9b650bb1eaf
deepak.kumar2 <pdeepak1255@gmail.com>
2025-06-18 08:58:29 +0530

    Draggable Modifiers: Box with constraints Demo

---
+++ app/src/main/java/com/example/superapp/mesurements/BoxWithConstraintsDemo.kt
@@ -0,0 +1,27 @@
+package com.example.superapp.mesurements
+
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.superapp.ui.theme.SuperAppTheme
+
+@Composable
+fun BoxWithConstraintDemo(modifier: Modifier = Modifier) {
+    BoxWithConstraints {
+        if(this.constraints.hasFixedWidth){
+            Text(text = "Fixed width")
+        } else {
+            Text(text = "dynamic width")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun BoxWithConstraintDemoPreview() {
+    SuperAppTheme {
+        BoxWithConstraintDemo()
+    }
+}
